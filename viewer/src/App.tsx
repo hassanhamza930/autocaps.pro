f5954176255641fa6ccb5f99084486666fb82216
Hamza Hassan <hassanhamza930@gmail.com>
2024-05-07 01:55:28 +0500

    Need to implement speech transcription or ideally take stuff from youtube etc.

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Player } from '@remotion/player'
@@ -8,25 +8,129 @@ function App() {
   const [count, setCount] = useState(0)
   const duration = 8
 
-  return (
-    <div style={{backgroundImage:`url("https://cdn.dribbble.com/users/600626/screenshots/2944155/loading_img.gif")`}} className='h-screen w-full bg-repeat bg-center'>
-      <div className='h-full w-full bg-purple-600/90 p-10 flex justify-start items-start backdrop-blur-xl'>
+  const [permission, setpermission] = useState(false);
+  const [stream, setStream] = useState(null);
+  const mediaRecorder = useRef(null);
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [audio, setAudio] = useState(null);
+  const [speech, setspeech] = useState("Hello World, Let's see how it goes");
+  const mimeType = "audio/webm";
 
-      <Player
-				autoPlay
-				loop
-        className='h-full rounded-xl opacity-90'
-				component={TextAnim}
-				durationInFrames={30*duration}
-        posterFillMode='player-size'
-        compositionHeight={1920}
-        compositionWidth={1080}
-        style={{ height: '100%' }}
-        fps={30}
-			/>
+
+
+  const startRecording = async () => {
+    setRecordingStatus("recording");
+    //create new Media recorder instance using the stream
+    const media = new MediaRecorder(stream, { mimeType: mimeType });
+    //set the MediaRecorder instance to the mediaRecorder ref
+    mediaRecorder.current = media;
+    //invokes the start method to start the recording process
+    mediaRecorder.current.start();
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      localAudioChunks.push(event.data);
+    };
+    setAudioChunks(localAudioChunks);
+  };
+
+
+  const stopRecording = () => {
+    setRecordingStatus("inactive");
+    //stops the recording instance
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      //creates a blob file from the audiochunks data
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      //creates a playable URL from the blob file.
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudio(audioUrl);
+      setAudioChunks([]);
+    };
+  };
+
+
+  const getMicrophonePermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        setpermission(true);
+        setStream(streamData);
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
+
+  useEffect(()=>{
+    getMicrophonePermission();
+    setTimeout(()=>{
+      setspeech("I mean i'm not the one to complain or anything but it's just been really hard these past couple of days");
+    },2000);
+  },[])
+
+
+  return (
+    <div style={{ backgroundImage: `url("https://cdn.dribbble.com/users/600626/screenshots/2944155/loading_img.gif")` }} className='h-screen w-full bg-repeat bg-center'>
+      <div className='h-full w-full bg-black/90 p-10 flex backdrop-blur-xl flex-row justify-start items-center gap-5'>
+
+        <Player
+          autoPlay
+          loop
+          className='h-full rounded-xl opacity-90 flex-none'
+          component={TextAnim}
+          durationInFrames={30 * duration}
+          posterFillMode='player-size'
+          compositionHeight={1920}
+          compositionWidth={1080}
+          style={{ height: '100%' }}
+          inputProps={{title:speech}}
+          fps={30}
+        />
+
+        <div className='h-full w-full bg-white/90 rounded-xl backdrop-blur-xl flex flex-col gap-5 justify-center items-center'>
+          <div className="audio-controls">
+            {!permission ? (
+              <button className='bg-yellow-400 text-black/90 shadow-md rounded-md px-6 py-2' onClick={getMicrophonePermission} type="button">
+                Get Microphone
+              </button>
+            ) : null}
+            {permission && recordingStatus === "inactive" ? (
+              <button className='bg-blue-600 text-white rounded-md px-6 py-2' onClick={startRecording} type="button">
+                {
+                  audio?
+                  "Record Again":
+                  "Start Recording"
+                }
+              </button>
+            ) : null}
+            {recordingStatus === "recording" ? (
+              <button className='bg-red-500 text-white rounded-md px-6 py-2' onClick={stopRecording} type="button">
+                Stop Recording
+              </button>
+            ) : null}
+          </div>
+
+          {audio ? (
+            <div className="audio-container flex flex-col justify-center items-center gap-5">
+              <audio src={audio} controls></audio>
+              <a className='px-6 text-xs py-2 rounded-full bg-black text-white/80 shadow-xl' download href={audio}>
+                Download Recording
+              </a>
+            </div>
+          ) : null}
+
+        </div>
 
       </div>
-      
+
 
     </div>
   )
--- viewer/src/remotion/textAnimComp.tsx
+++ viewer/src/remotion/textAnimComp.tsx
@@ -3,20 +3,16 @@ import { z } from 'zod';
 import { zColor } from '@remotion/zod-types';
 import { useEffect, useRef, useState } from 'react';
 
-export const myCompSchema = z.object({
 
-});
-
-export const TextAnim: React.FC = ({
-}) => {
+export const TextAnim: React.FC = (props:{title:string}) => {
 
 
 	const frame = useCurrentFrame();
 	const animDuration = 10;
 	const animDelay = 5;
-	const title = "I Got Cold Called by an AI and didn't realize it untill it glitched out 🤯🤯🤯🤯";
 	const signature=interpolate(frame,[100,120],[0,1])
 
+	useEffect(()=>{},[props.title])
 
 	//need to add functionality to limit words on screen.
 
@@ -44,11 +40,11 @@ export const TextAnim: React.FC = ({
 
 
 	return (
-		<div className='h-screen w-full bg-yellow-500'>
-			<AbsoluteFill className="relative z-20 bg-gradient-to-b from-yellow-400 via-yellow-500 to-orange-500/60 backdrop-blur-sm flex items-start justify-start p-[5%] py-[10%] h-screen w-full">
-				<div className='relative text-9xl font-bold w-full text-start text-black/70 flex flex-wrap gap-x-6 gap-y-5 justify-start items-start '>
+		<div className='h-screen w-full'>
+			<AbsoluteFill className="relative z-20 bg-gradient-to-b from-blue-600 via-blue-500 to-yellow-400 backdrop-blur-sm flex items-start justify-start p-[5%] py-[10%] h-screen w-full">
+				<div className='relative text-9xl font-bold w-full text-start text-white/90 flex flex-wrap gap-x-6 gap-y-5 justify-start items-start '>
 					{
-						title.split(" ").map((word, index) => {
+						props.title.split(" ").map((word, index) => {
 
 							return (
 								frame > ((index + 1) * animDelay) &&
@@ -66,8 +62,8 @@ export const TextAnim: React.FC = ({
 
 
 			</AbsoluteFill>
-			<div style={{backgroundPositionY:frame,backgroundPositionX:-frame*3}} className='h-full w-full flex bg-cover bg-center justify-end items-end p-20 z-10 bg-[url("https://media.meer.com/attachments/d3b3f598d670aa511dc81ea8fdb43aa8c7d9049d/store/fill/1090/613/115e0b937c2f533e9ae2d71f25c0c14d41a6a1411f6abe00671aef2e3840/Sophia-Hanson-Robotics-Ltd-speaking-at-the-AI-for-GOOD-Global-Summit-ITU-Geneva-Switzerland-7-9.jpg")] '>
-			</div>
+			{/* <div style={{backgroundPositionY:frame,backgroundPositionX:-frame*3,backgroundImage:`url('https://www.elegantthemes.com/blog/wp-content/uploads/2023/06/What-is-AI-1.jpg')`}} className='h-full w-full flex bg-cover bg-center justify-end items-end p-20 z-10 absolute'>
+			</div> */}
 		</div>
 	);
 };
